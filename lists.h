1abe0c1eb561116f9a09e3dc2c7493fbc389cec2
arizzen <a1.arizzen@gmail.com>
2023-01-05 14:35:57 -0500

    alxa

--- lists.h
+++ lists.h
@@ -3,7 +3,14 @@
 
 #include <stdlib.h>
 
-
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct listint_s
 {
 	int n;
@@ -16,3 +23,4 @@ void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
+
